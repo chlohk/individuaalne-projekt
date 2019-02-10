2219fc3ccd2f0e82f0e87d1fd83325ba3433674e
Christer <chlohk@ttu.ee>
2019-02-10 23:07:52 +0200

    tarkvaratehnika

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -1,14 +1,18 @@
 package hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+
+    @GetMapping("/api")
+    public Map<String, String> h() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "Christer");
+        return map;
     }
-    
 }
